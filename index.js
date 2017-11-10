a8ffee02e0a5e528f5ac99879ae07d753635d027
Alex Johnson <alex@gritdesign.co>
2017-11-10 21:49:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ while (i < facts.length) {
 function iLoveTheBeatles(n) {
   var array = []
   do {
-    array.push(`I love the Beatles`)
+    array.push(`I love the Beatles!`)
     n++
   }
   while (n < 15)
